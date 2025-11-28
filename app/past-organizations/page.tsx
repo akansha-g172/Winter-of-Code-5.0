daaa1c21774f59054058ed472ec296f49fdf211e
HARSH RAJ <harshraj250106@gmail.com>
2025-11-28 15:00:19 +0530

    Made PAST ORGANIZATIONS Page

---
+++ app/past-organizations/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import PastOrganizations from '@/components/past-organizations';
+
+export default function PastOrganizationsPage() {
+    return (
+        <main>
+            <PastOrganizations />
+        </main>
+    );
+}
--- components/footer.tsx
+++ components/footer.tsx
@@ -11,9 +11,10 @@ import mailbox from '../public/mailbox.svg'
 import submit from '../public/submit.svg'
 
 const resources = [
-    {title:"Organisation Brochure", link:"https://drive.google.com/file/d/117rMEB4imPUtNh0RtCQ-A-FK5wGMWM-y/view?usp=sharing"},
-    {title:"Sponsorship Brochure", link:"https://drive.google.com/file/d/1vOyyjSdUysjeUrpb87Ubb-Rkl542M9v2/view?usp=sharing"},
-    {title:"WoC 5.0 Policies & Guidelines", link:"https://www.notion.so/Winter-of-Code-5-0-Official-Policies-Legal-Guidelines-2a6489c63bf4806593abcfd35a481976?source=copy_link"},
+    { title: "Organisation Brochure", link: "https://drive.google.com/file/d/117rMEB4imPUtNh0RtCQ-A-FK5wGMWM-y/view?usp=sharing" },
+    { title: "Sponsorship Brochure", link: "https://drive.google.com/file/d/1vOyyjSdUysjeUrpb87Ubb-Rkl542M9v2/view?usp=sharing" },
+    { title: "WoC 5.0 Policies & Guidelines", link: "https://www.notion.so/Winter-of-Code-5-0-Official-Policies-Legal-Guidelines-2a6489c63bf4806593abcfd35a481976?source=copy_link" },
+    { title: "Past Organizations", link: "/past-organizations" },
 ]
 
 const Footer = () => {
@@ -25,32 +26,32 @@ const Footer = () => {
 
     return (
         <div className="flex pb-[1%] px-[5%] w-full justify-between"
-             style={{backgroundImage: `url(${footer.src})`, backgroundSize:"cover"}}>
+            style={{ backgroundImage: `url(${footer.src})`, backgroundSize: "cover" }}>
 
             <div className="flex-col flex w-[25%] justify-between m-[1%]">
                 <div className="w-full text-scale-305 text-black font-kleemax border-outline">
-                    WOC                    
+                    WOC
                 </div>
 
                 <div className="flex gap-[4%] items-center justify-start">
                     <Link href="https://www.instagram.com/gdgiiitkalyani/" target="_blank" className="w-[22%]">
                         <Image className="bg-gradient-radial from-white via-white via-60% to-transparent to-60% hover:scale-110 duration-700"
-                               src={instagram} alt="instagram"/>
+                            src={instagram} alt="instagram" />
                     </Link>
 
                     <Link href="https://x.com/gdsciiitkalyani" target="_blank" className="w-[22%]">
                         <Image className="bg-gradient-radial from-white via-white via-60% to-transparent to-60% hover:scale-110 duration-700"
-                               src={twitter} alt="twitter"/>
+                            src={twitter} alt="twitter" />
                     </Link>
 
                     <Link href="https://discord.gg/BP6rPGfujM" target="_blank" className="w-[22%]">
                         <Image className="p-[7.5%] hover:scale-110 duration-700"
-                               src={discord} alt="discord"/>
+                            src={discord} alt="discord" />
                     </Link>
 
                     <Link href="https://www.linkedin.com/company/gdsc-iiit-kalyani" target="_blank" className="w-[19%] relative">
                         <Image className="bg-gradient-radial from-white via-white via-65% to-transparent to-65% hover:scale-110 duration-700"
-                               src={linkedin} alt="linkedin"/>
+                            src={linkedin} alt="linkedin" />
                     </Link>
                 </div>
             </div>
@@ -62,7 +63,7 @@ const Footer = () => {
 
                 {resources.map((resource, index) => (
                     <Link href={resource.link} target="_blank" key={index}
-                          className="text-scale-30-5 text-white font-chakra w-[90%] m-[1%] hover:scale-110 duration-700">
+                        className="text-scale-30-5 text-white font-chakra w-[90%] m-[1%] hover:scale-110 duration-700">
                         {resource.title}
                     </Link>
                 ))}
@@ -79,7 +80,7 @@ const Footer = () => {
 
                 <div className="flex w-full justify-between">
                     <div className="relative w-[80%]">
-                        <Image className="w-full" src={mailbox} alt="mailbox"/>
+                        <Image className="w-full" src={mailbox} alt="mailbox" />
                         <input
                             type="text"
                             id="terminal-input"
@@ -89,8 +90,8 @@ const Footer = () => {
                     </div>
 
                     <Image className="w-[16%] cursor-pointer"
-                           src={submit} alt="submit"
-                           onClick={handleClick}/>
+                        src={submit} alt="submit"
+                        onClick={handleClick} />
                 </div>
             </div>
         </div>
